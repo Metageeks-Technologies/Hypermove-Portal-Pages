abe2c6378f6395954420f3f97cf63403bb15cef0
shiva <ms454431@gmail.com>
2024-04-15 20:57:55 +0530

    deploy.yml added

--- src/controller/tournamentController.ts
+++ src/controller/tournamentController.ts
@@ -27,23 +27,23 @@ export const getTournament = catchAsyncError(async (req, res, next) => {
 export const addWinner = catchAsyncError(async (req, res, next) => {
 
     const { tournamentId, userId, userWalletAddress, headShot, kills } = req.body;
-    if (!tournamentId || !userId || !userWalletAddress || !headShot || !kills) {
+    if (!tournamentId || !userId || !userWalletAddress || headShot !=== undefined || kills === undefined) {
         return next(new ErrorHandler('Please provide tournamentId,headShot,kills and userId', 400));
     }
-    const tournament = await Tournament.findById(tournamentId);
-    if (!tournament) {
-        return next(new ErrorHandler('Tournament not found', 404));
-    }
-    const winner = {
-        position: tournament.winners.length + 1,
-        winnerId: userId,
-        walletAddress: userWalletAddress,
-        headShot,
-        kills
-    }
-    tournament.winners.push(winner);
-    await tournament.save({ validateBeforeSave: false });
+const tournament = await Tournament.findById(tournamentId);
+if (!tournament) {
+    return next(new ErrorHandler('Tournament not found', 404));
+}
+const winner = {
+    position: tournament.winners.length + 1,
+    winnerId: userId,
+    walletAddress: userWalletAddress,
+    headShot,
+    kills
+}
+tournament.winners.push(winner);
+await tournament.save({ validateBeforeSave: false });
 
-    res.status(200).json({ success: true, message: `${userId} is winner` });
+res.status(200).json({ success: true, message: `${userId} is winner` });
 });
 
